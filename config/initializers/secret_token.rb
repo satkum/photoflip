9999a1313d40736d244e892f010d82a42c8fea59
toloumi <t_louverture_m@yahoo.com>
2013-09-21 11:20:06 -0500

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PhotoFlip::Application.config.secret_key_base = '6d436f87e670118d09cbdfe195b750bf435f1c03fc4d9b26d43a2bcbf8e4f6b1f96bbbdd27b7339be7c2c6f75bd104000abfc9f89ed6a987483f6ecc56db2d5c'
