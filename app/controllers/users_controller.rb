85abffb44de521c9ff414bc5045ad543d7a79c73
toloumi <t_louverture_m@yahoo.com>
2013-09-22 19:22:28 -0500

    User authentification working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,32 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    if @user.save
+       render 'login'    
+    else
+      render 'new'
+    end
   end
 
   def new
     @user = User.new
   end
  
-   def login
+  def login
   end
+    
+  def create
+    @user = User.new(user_params)
+    if @user.save
+
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+        params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
